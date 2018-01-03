a8e1a9eebd0020b67867e95ec9474f2da3b6809f
Brhawh <benn.r.henderson@gmail.com>
2018-01-03 22:23:59 +0000

    Player now moves towards mouse using physics.
    Instead of changing the transform position we now apply a force to the rigidbody to make it move towards the mouse.
    The rigidbody for the player has dampening so it slows down if no force is applied.
    Collisions with static objects work better, no jumping.

--- WallBreaker/Assets/FollowMouse.cs
+++ WallBreaker/Assets/FollowMouse.cs
@@ -6,16 +6,18 @@ public class FollowMouse : MonoBehaviour {
 
 	private Vector3 mousePosition;
 	public float moveSpeed = 0.1f;
+	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		mousePosition = Input.mousePosition;
 		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
-		transform.position = Vector2.Lerp (transform.position, mousePosition, moveSpeed);
+		Vector2 direction = (mousePosition - transform.position).normalized;
+		rb.AddForce (direction * moveSpeed);
 	}
 }
