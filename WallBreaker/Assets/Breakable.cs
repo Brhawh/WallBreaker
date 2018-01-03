0d4c75420663543a61d00d789720252f66d02771
Brhawh <benn.r.henderson@gmail.com>
2018-01-03 23:33:25 +0000

    Breakable Objects created.
    Added another sprite object using placeholder image to represent a breakable object.
    Object checks for collision and get's destroyed if other object is moving fast enough.

---
+++ WallBreaker/Assets/Breakable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+	public float breakingVelocityMagnitude;
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		print ("There was a collision!");
+		if (coll.relativeVelocity.magnitude > breakingVelocityMagnitude) {
+			GameObject.Destroy (gameObject);
+		}
+	}
+}
