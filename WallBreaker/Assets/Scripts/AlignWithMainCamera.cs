4d5e0517c7ba3717049bcba31da451e13a8004e9
Brhawh <benn.r.henderson@gmail.com>
2018-01-15 23:08:41 +0000

    Created border based on screen size. Changed player sprite to unity circle sprite.
    Created 4 sub objects of main camera that position themselves on each edge of the screen to act as a border.
    Created ball sprite using unity sprite creator.
    Adjusted player circle collider to fit new sprite.

---
+++ WallBreaker/Assets/Scripts/AlignWithMainCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CameraEdge {Top, Bottom, Left, Right};
+
+public class AlignWithMainCamera : MonoBehaviour {
+
+	public CameraEdge cameraEdge;
+	// Use this for initialization
+	void Start () {
+		float height = Camera.main.orthographicSize * 2;
+		float width = height * Screen.width / Screen.height;
+		switch (cameraEdge) {
+		case CameraEdge.Top:
+		case CameraEdge.Bottom:
+			transform.localScale = new Vector2 (width, 1);
+			break;
+		case CameraEdge.Left:
+		case CameraEdge.Right:
+			transform.localScale = new Vector2 (1, height);
+			break;
+		}
+		float topY = height / 2 + transform.localScale.y / 2;
+		float leftX = -width / 2 - transform.localScale.x / 2;
+		switch (cameraEdge) {
+		case CameraEdge.Top:
+			transform.position = new Vector2 (0, topY);
+			break;
+		case CameraEdge.Bottom:
+			transform.position = new Vector2 (0, -topY);
+			break;
+		case CameraEdge.Left:
+			transform.position = new Vector2 (leftX, 0);
+			break;
+		case CameraEdge.Right:
+			transform.position = new Vector2 (-leftX, 0);
+			break;
+		}
+	}
+}
