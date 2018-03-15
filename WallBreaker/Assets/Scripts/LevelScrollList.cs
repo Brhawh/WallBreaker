8f00e8b5155df7deb2dfe402f66077d768dbd27a
Brhawh <benn.henderson@instil.co>
2018-03-15 00:11:00 +0000

    Created scrollable list view for level selection.
    Created the view using UI elements.
    Uses an object pool when creating buttons.
    Adds buttons on script start based on items in a list.

---
+++ WallBreaker/Assets/Scripts/LevelScrollList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Item {
+	public string itemName;
+}
+
+public class LevelScrollList : MonoBehaviour {
+
+	public List<Item> itemList;
+	public Transform contentPanel;
+	public SimpleObjectPool buttonObjectPool;
+
+	// Use this for initialization
+	void Start () {
+		RefreshDisplay ();
+	}
+
+	public void RefreshDisplay() {
+		AddButtons ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void AddButtons() {
+		for (int i = 0; i < itemList.Count; i++) {
+			Item item = itemList [i];
+			GameObject newButton = buttonObjectPool.GetObject ();
+			newButton.transform.SetParent (contentPanel);
+
+			LevelSelectButton levelButton = newButton.GetComponent<LevelSelectButton> ();
+			levelButton.SetUp (item, this);
+		}
+	}
+}
---
+++ WallBreaker/Assets/Scripts/LevelSelectButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectButton : MonoBehaviour {
+
+	public Button button;
+	public Text levelNameLabel;
+
+	private Item item;
+	private LevelScrollList scrollList;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void SetUp(Item currentItem, LevelScrollList currentList){
+		Debug.Log (currentItem);
+		Debug.Log (currentList);
+		item = currentItem;
+		levelNameLabel.text = currentItem.itemName;
+
+		scrollList = currentList;
+	}
+}
